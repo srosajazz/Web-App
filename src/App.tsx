9fadea5ad5f9406b3bad193f56ad7946c721395e
Sergio Rosa <scarlosjazz@gmail.com>
2020-08-04 08:59:30 -0400

    chore: add landingpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from "react";
+import Landing from "./pages/Landing";
 import "./assets/styles/global.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-      <button type="button">Click on me!</button>
-    </div>
-  );
+  return <Landing />;
 }
 
 export default App;
---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import logoImg from "../../assets/images/logo.svg";
+import landingImg from "../../assets/images/landing.svg";
+import studyIcon from "../../assets/images/icons/study.svg";
+import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
+import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
+
+function Landing() {
+  return (
+    <div id="page-landing">
+      <div id="page-lading-content" className="container">
+        <div className="logo-container">
+          <img src={logoImg} alt="logo" />
+          <h2>Your online platform for study.</h2>
+        </div>
+        <img src={landingImg} alt="Platform for study" className="hero-image" />
+
+        {/* Buttons */}
+        <div className="buttons-container">
+          <a href="" className="study">
+            <img src={studyIcon} alt="Study" />
+            Study
+          </a>
+
+          <a href="" className="give-class">
+            <img src={giveClassesIcon} alt="Study" />
+            Study
+          </a>
+        </div>
+        {/*  */}
+        <span className="total-connections">
+          Total of 400 conections!{" "}
+          <img src={purpleHeartIcon} alt="purple heart" />
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
